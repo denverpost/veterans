764ff2ed0b273a82b1fbea48827366b3080514c1
schneidan <dan@scrollwright.com>
2015-11-06 12:25:44 -0700

    Remove slick.js since it's not used; fix title CSS; make text slightly lighter.

--- js/functions.js
+++ js/functions.js
@@ -10,8 +10,6 @@ var adsloaded = [];
 var showAds = true; //show slide-up leaderboards at bottom
 var slideAds = 3; //number of times to slide up a leaderboard
 var titleFade = true; //whether to fade the Denver Post logo in the top-bar to show the "DP" and a text title
-var galleries = [];
-$('.centergallery').each(function(i,e) { galleries.push('#'+$(e).attr('id')) }); //div/section IDs of galleries to instantiate (must be a div like #photos and have a child, the gallery itself, with the same ID plus 'gallery' -- i.e. #photosgallery)
 
 function revealSocial(type,link,title,image,desc,twvia,twrel) {
     title = typeof title !== 'undefined' ? title : false;
@@ -44,34 +42,8 @@ function revealCredits() {
     $('#credits').foundation('reveal', 'open');
 }
 
-function revealSlides(galleries) {
-    for (key in galleries) {
-        if (galleriesLoaded.indexOf(galleries[key]) == -1) {
-            $(galleries[key]).find('img').unveil();
-            $(galleries[key]).slick({
-                centerMode: true,
-                centerPadding: '15%',
-                slidesToShow: 1,
-                prevArrow: '<button type="button" class="slick-prev"></button>',
-                nextArrow: '<button type="button" class="slick-next"></button>',
-                responsive: [{
-                    breakpoint: 800,
-                    settings: {
-                        arrows: true,
-                        centerMode: true,
-                        centerPadding: '8%',
-                        slidesToShow: 1
-                    }
-                }]
-            });
-            galleriesLoaded.push(galleries[key]);
-        }
-    }
-}
-
 function checkHash() {
     if (window.location.hash) {
-        revealSlides(galleries);
         var hash = window.location.hash;
         if ($(hash).hasClass('hide')) {
             toggleSidebar(hash,hash + 'link');
@@ -262,7 +234,6 @@ $(window).scroll(function() {
 setInterval(function() {
     if (didScroll) {
         checkFade();
-        revealSlides(galleries);
         checkAdPos();
     }
 },250);
